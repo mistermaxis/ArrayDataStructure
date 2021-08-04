8b22324716e494cea7c56072b574f1e3039f2054
Maximiliano Diaz <maxi_diaz@mail.com>
2021-08-04 18:46:07 -0300

    Change h to hpp in header extensions.

--- ArrayDataStructure/Scores.hpp
+++ ArrayDataStructure/Scores.hpp
@@ -1,12 +1,12 @@
 #pragma once
 #include <memory>
-#include "GameEntry.h"
+#include "GameEntry.hpp"
 
 class Scores
 {
 public:
 	Scores(size_t size = 10);
-	void add(const GameEntry& entry);
+	void add(const GameEntry &entry);
 	GameEntry remove(index_t index) {}
 	void printEntries();
 
@@ -16,30 +16,39 @@ private:
 	std::unique_ptr<GameEntry[]> entriesList;
 };
 
-Scores::Scores(size_t size) {
+Scores::Scores(size_t size)
+{
 	maxEntries = size;
 	entriesList = std::make_unique<GameEntry[]>(maxEntries);
 	entries = 0;
 }
 
-void Scores::add(const GameEntry& entry) {
-	if (entries == 0) {
+void Scores::add(const GameEntry &entry)
+{
+	if (entries == 0)
+	{
 
 		entriesList[entries] = entry;
 		entries++;
 	}
-	else {
-		if (entries <= maxEntries) {
+	else
+	{
+		if (entries <= maxEntries)
+		{
 
-			if(entry.getScore() > entriesList[entries -1].getScore()) {
+			if (entry.getScore() > entriesList[entries - 1].getScore())
+			{
 				int i;
-				for (i = entries - 1; entry.getScore() > entriesList[i].getScore() && i >= 0; i--) {
-					if ((i + 1) != maxEntries) {
+				for (i = entries - 1; entry.getScore() > entriesList[i].getScore() && i >= 0; i--)
+				{
+					if ((i + 1) != maxEntries)
+					{
 						entriesList[i + 1] = entriesList[i];
 					}
 				}
 				entriesList[i + 1] = entry;
-				if (entries < maxEntries) {
+				if (entries < maxEntries)
+				{
 					entries++;
 				}
 			}
@@ -47,9 +56,11 @@ void Scores::add(const GameEntry& entry) {
 	}
 }
 
-void Scores::printEntries() {
-	for (int i = 0; i < entries; i++) {
+void Scores::printEntries()
+{
+	for (int i = 0; i < entries; i++)
+	{
 		std::cout << "Name: " << entriesList[i].getName() << "\nScore: "
-			<< entriesList[i].getScore() << "\n";
+				  << entriesList[i].getScore() << "\n";
 	}
 }
--- ArrayDataStructure/main.cpp
+++ ArrayDataStructure/main.cpp
@@ -1,24 +1,24 @@
-#include "Scores.h"
+#include "Scores.hpp"
 
 int main()
 {
 	Scores scores;
 
-	scores.add(GameEntry("John", 339));
-	scores.add(GameEntry("Sam", 150));
-	scores.add(GameEntry("Ian", 179));
-	scores.add(GameEntry("Sandra", 255));
-	scores.add(GameEntry("Max", 472));
-	scores.add(GameEntry("Susan", 420));
-	scores.add(GameEntry("Monica", 777));
-	scores.add(GameEntry("Jane", 563));
-	scores.add(GameEntry("Maria", 357));
-	scores.add(GameEntry("Jessica", 159));
-	scores.add(GameEntry("Michael", 852));
-	scores.add(GameEntry("Jane", 349));
-	scores.add(GameEntry("Lisa", 673));
-	scores.add(GameEntry("Justin", 829));
-	scores.add(GameEntry("Charles", 381));
+	scores.add(GameEntry("John", 339U));
+	scores.add(GameEntry("Sam", 150U));
+	scores.add(GameEntry("Ian", 179U));
+	scores.add(GameEntry("Sandra", 255U));
+	scores.add(GameEntry("Max", 472U));
+	scores.add(GameEntry("Susan", 420U));
+	scores.add(GameEntry("Monica", 777U));
+	scores.add(GameEntry("Jane", 563U));
+	scores.add(GameEntry("Maria", 357U));
+	scores.add(GameEntry("Jessica", 159U));
+	scores.add(GameEntry("Michael", 852U));
+	scores.add(GameEntry("Jane", 349U));
+	scores.add(GameEntry("Lisa", 673U));
+	scores.add(GameEntry("Justin", 829U));
+	scores.add(GameEntry("Charles", 381Ugi));
 
 	scores.printEntries();
 
