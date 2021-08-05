5f252e9f2514ff34e43c505f8dc3c533514cb8fa
mistermaxis <maximiliano_diaz@outlook.com.ar>
2021-08-05 02:28:18 -0300

    Update dep files.

--- dep/Scores.d
+++ dep/Scores.d
@@ -1,1 +1,3 @@
-obj/Scores.o: src/Scores.cpp src/../include/Scores.hpp
+obj/Scores.o: src/Scores.cpp src/../include/Scores.hpp \
+ src/../include/../include/GameEntry.hpp \
+ src/../include/../include/../include/Typedefs.hpp
